180972a3076fe6ddd2d3d1102ab827dd099a59aa
karanja03 <wambuik03@gmail.com>
2023-06-13 22:07:22 +0300

    Completed arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
-// Write your code here
+var multiply;
+const num1 = 2;
+const num2 = 31;
+multiply = num1 * num2 ;
+console.log (multiply);
+
+var random;
+random = Number.isInteger(Math.random() *10 ) + 1;
+console.log (random);
+
+var mod;
+const num3 = 24;
+const num4 = 5;
+mod = num3 % num4 ;
+console.log (mod);
+ 
+
+
+
+var max =(1, 2, 3, 4, 20);
+Math.max(max);
+console.log(max);
+
+
+
